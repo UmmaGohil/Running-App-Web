f318c1994bd629e0fa31ff528671a83a17e2724a
u-gohil <756D6D61@gmail.com>
2021-01-06 00:17:44 +0000

    Temp border

--- src/template/page.js
+++ src/template/page.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const Page = ({ children, bgColour }) => {
-  const pageVar = `w-screen h-screen bg-${bgColour}-500`
-  return <div className={pageVar}>{children}</div>
+  const pageVar = `w-screen h-screen bg-${bgColour}-500 p-8`
+  return (
+    <div className={pageVar}>
+      <div className="rounded-lg border-2 border-white ">{children}</div>
+    </div>
+  )
 }
 
 export default Page
