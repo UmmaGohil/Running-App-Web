1709000306609a2d9e31e118f1cbab0a0d935afd
um-ma <umgo0510@gmail.com>
2021-02-03 12:56:16 +0000

    Removed unused code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { StoreProvider } from './store'
 import ReactDOM from 'react-dom'
 import { renderRoutes } from 'react-router-config'
 import routes from './config/routes'
@@ -8,9 +7,7 @@ import './styles/tailwind.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <StoreProvider>
       <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>
-    </StoreProvider>
   </React.StrictMode>,
   document.getElementById('app')
 )
