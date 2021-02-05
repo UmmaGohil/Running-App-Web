b0d004bc907dd1cd099c5f4706f33547857e3d4d
um-ma <umgo0510@gmail.com>
2021-02-05 17:21:18 +0000

    Run details styles

--- src/components/RunDetails.js
+++ src/components/RunDetails.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const RunDetails = () => {
   return (
-    <div>
-      For the view page to see time, distance and date [on button click]
+    <div className="bg-white rounded-md p-12 text-paleYellow-500">
+      <h1>Time</h1>
+      <p>1:05:23 </p>
+      <h1>Distance</h1>
+      <p>10.02 km </p>
+      <h1>Date</h1>
+      <p>01/01/21</p>
     </div>
   )
 }
