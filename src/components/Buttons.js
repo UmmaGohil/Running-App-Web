aef4dae2fbe44a009703dcfed75889173bfec71a
um-ma <umgo0510@gmail.com>
2021-02-09 17:21:02 +0000

    Button styling

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const Buttons = ({ dateText, title, buttonFontColour }) => {
-  const styles = `bg-white text-${buttonFontColour}-500 font-bold py-2 px-4 rounded-md  items-center w-1/7`
+  const styles = `bg-white text-${buttonFontColour}-500 font-bold py-2 px-4 rounded-md mb-5vh items-center w-1/7`
 
   return (
-    <button class={styles}>
+    <button className={styles}>
       {title}
 
       {'|' + dateText}
