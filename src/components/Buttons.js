3f6dfec5746aa8c82e7c8a0966d508125d1a68e4
um-ma <umgo0510@gmail.com>
2021-02-11 18:40:19 +0000

    Basic onClick

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -3,10 +3,12 @@ import React from 'react'
 const Buttons = ({ dateText, title, buttonFontColour }) => {
   const styles = `bg-white text-${buttonFontColour}-500 font-bold py-2 px-4 rounded-md mb-5vh items-center w-1/7`
 
+  const test = () => {
+    console.log('testing')
+  }
   return (
-    <button className={styles}>
+    <button className={styles} onClick={test}>
       {title}
-
       {dateText}
     </button>
   )
